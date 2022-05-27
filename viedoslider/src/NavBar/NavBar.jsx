4a572e0ca9c8a796e1153c08a37bd60755ecda7b
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-27 13:01:51 +0530

    updated

---
+++ viedoslider/src/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./NavBar.css"
+
+export default function NavBar() {
+  return (
+    <div className='NavBar'>
+        <div>
+          Home
+      </div>
+
+      <div>
+          Onboarding
+      </div>
+      
+    </div>
+  )
+}
